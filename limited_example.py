ca83c8f3fb0c278d685f9967135a2f8622b4140b
Steven Mackey <stevester94@gmail.com>
2021-04-22 20:54:13 -0700

    whatever change to limited example

--- limited_example.py
+++ limited_example.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-# This is just a toy to test out basic keras usage. We are training an identity function
+# Toy example where we aren't even using a one hot encoding.
 
 
 import datasetaccessor
@@ -14,17 +14,22 @@ tf.random.set_seed(1337)
 vdsa = datasetaccessor.SymbolDatasetAccessor(
     day_to_get=[2],
     transmitter_id_to_get=[1,2],
+    #transmitter_id_to_get=[1],
     tfrecords_path="../csc500-dataset-preprocessor/symbol_tfrecords/")
 
 
 ds = vdsa.get_dataset()
 ds = ds.map(lambda inp: ( inp["frequency_domain_IQ"], inp["transmitter_id"]))
+#ds = ds.take(100)
 ds = ds.batch(100)
 
-
+# Note that even with this input shape, keras still expects the input to be batched
 inputs  = keras.Input(shape=(2,48))
 x = keras.layers.Flatten()(inputs)
-x = keras.layers.Dense(96)(x)
+x = keras.layers.Dense(150)(x)
+x = keras.layers.Dense(150)(x)
+x = keras.layers.Dense(150)(x)
+x = keras.layers.Dense(150)(x)
 outputs = keras.layers.Dense(1)(x)
 
 model = keras.Model(inputs=inputs, outputs=outputs, name="steves_model")
