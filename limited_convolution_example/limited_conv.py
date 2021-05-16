8046d042098b29a9ce2fe809181a96958d87a689
Steven Mackey <stevester94@gmail.com>
2021-05-16 14:26:33 -0700

    Its training

--- limited_convolution_example/limited_conv.py
+++ limited_convolution_example/limited_conv.py
@@ -3,13 +3,14 @@
 # This is just a toy to test out basic keras usage. We are training an identity function
 
 
-import sys
+import sys, os
 from datetime import datetime
 
 from steves_utils.graphing import plot_confusion_matrix, plot_loss_curve, save_confusion_matrix, save_loss_curve
 from steves_utils import utils
 from steves_utils.ORACLE.simple_oracle_dataset_factory import Simple_ORACLE_Dataset_Factory
 from steves_utils.ORACLE.utils import ORIGINAL_PAPER_SAMPLES_PER_CHUNK, ALL_SERIAL_NUMBERS
+from steves_utils.ORACLE.shuffled_dataset_accessor import Shuffled_Dataset_Factory
 
 import tensorflow as tf
 import tensorflow.keras.models as models
@@ -23,37 +24,20 @@ if __name__ == "__main__":
 
     # Hyper Parameters
     RANGE   = len(ALL_SERIAL_NUMBERS)
-    EPOCHS  = 2
+    EPOCHS  = 20
     DROPOUT = 0.5 # [0,1], the chance to drop an input
-    BATCH = 1000
 
     TRAIN_SPLIT, VAL_SPLIT, TEST_SPLIT = (0.6, 0.2, 0.2)
 
-    
-    ds, cardinality = Simple_ORACLE_Dataset_Factory(
-        ORIGINAL_PAPER_SAMPLES_PER_CHUNK, 
-        runs_to_get=[1],
-        distances_to_get=[8],
-        serial_numbers_to_get=ALL_SERIAL_NUMBERS[:3]
-    )
+    path = os.path.join(utils.get_datasets_base_path(), "all_shuffled", "output")
+    print(utils.get_datasets_base_path())
+    print(path)
+    datasets = Shuffled_Dataset_Factory(path, train_val_test_splits=(0.6, 0.2, 0.2))
 
-    print("Total Examples:", cardinality)
-    print("That's {}GB of data (at least)".format( cardinality * ORIGINAL_PAPER_SAMPLES_PER_CHUNK * 2 * 8 / 1024 / 1024 / 1024))
+    train_ds = datasets["train_ds"]
+    val_ds = datasets["val_ds"]
+    test_ds = datasets["test_ds"]    
 
-    input("Press enter to continue calmly")
-
-    num_train = int(cardinality * TRAIN_SPLIT)
-    num_val = int(cardinality * VAL_SPLIT)
-    num_test = int(cardinality * TEST_SPLIT)
-
-    ds = ds.shuffle(cardinality)
-
-    train_ds = ds.take(num_train)
-    val_ds = ds.skip(num_train).take(num_val)
-    test_ds = ds.skip(num_train+num_val).take(num_test)
-    
-
-    # train_ds = train_ds.unbatch().filter(lambda freq_iq, day, transmitter_id, transmission_id, symbol_index_in_file: day == 1).batch(100)
     train_ds = train_ds.map(
         lambda x: (x["IQ"],tf.one_hot(x["serial_number_id"], RANGE)),
         num_parallel_calls=tf.data.AUTOTUNE,
@@ -72,46 +56,9 @@ if __name__ == "__main__":
         deterministic=True
     )
 
-    # train_ds = train_ds.map(
-    #     lambda freq_iq, day, transmitter_id, transmission_id, symbol_index_in_file: (freq_iq, transmitter_id),
-    #     num_parallel_calls=tf.data.AUTOTUNE,
-    #     deterministic=True
-    # )
-
-    # val_ds = val_ds.map(
-    #     lambda freq_iq, day, transmitter_id, transmission_id, symbol_index_in_file: (freq_iq, transmitter_id),
-    #     num_parallel_calls=tf.data.AUTOTUNE,
-    #     deterministic=True
-    # )
-
-    # test_ds = test_ds.map(
-    #     lambda freq_iq, day, transmitter_id, transmission_id, symbol_index_in_file: (freq_iq, transmitter_id),
-    #     num_parallel_calls=tf.data.AUTOTUNE,
-    #     deterministic=True
-    # )
-
-    # train_ds = train_ds.prefetch(100).unbatch().batch(5000)
-    # train_ds = train_ds.prefetch(100).take(2)
-    # val_ds   = val_ds.prefetch(100).take(2)
-    # test_ds  = test_ds.prefetch(100).take(2)
-
-    # train_ds = train_ds.prefetch(100).take(1).cache().repeat(100000).repeat(100000)
-    # train_ds = train_ds.batch(100).map(lambda x,y: (tf.reshape(x, [10000, 2, 48]), tf.reshape(y, [10000, 21]))).prefetch(100)
-    # train_ds = train_ds.caprefetch(100)
-    # val_ds   = val_ds.prefetch(100)
-    # test_ds  = test_ds.prefetch(100)
-
-    train_ds = train_ds.batch(BATCH)
-    val_ds   = val_ds.batch(BATCH)
-    test_ds  = test_ds.batch(BATCH)
-
-
-    train_ds = train_ds.cache().shuffle(num_train, reshuffle_each_iteration=True)
-    val_ds   = val_ds.cache()
-    test_ds  = test_ds.cache()
-
-    # for e in train_ds.unbatch():
-    #     print(e)
+    train_ds = train_ds.prefetch(100)
+    val_ds   = val_ds.prefetch(100)
+    test_ds  = test_ds.prefetch(100)
 
     inputs  = keras.Input(shape=(2,ORIGINAL_PAPER_SAMPLES_PER_CHUNK))
 
