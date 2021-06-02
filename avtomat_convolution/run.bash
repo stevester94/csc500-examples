bdec88a07bfd8252f0fa8f199f672c523675fe07
Steven Mackey <stevester94@gmail.com>
2021-06-01 20:35:01 -0700

    De-prioritize learning rate

--- avtomat_convolution/run.bash
+++ avtomat_convolution/run.bash
@@ -8,9 +8,9 @@ original_batch_size=100
 patience=10
 
 for epochs in 10 300; do
+for learning_rate in 0.0001 0.001; do
 for desired_batch_size in 128 256 512; do
 for distance in 2 4 8 14 20 26 32 38 44 50 56 62; do
-for learning_rate in 0.0001 0.001; do
     experiment_name=avtomat_distance-${distance}_learningRate-${learning_rate}_batch-${desired_batch_size}_epochs-${epochs}_patience-$patience
     echo "Begin $experiment_name" | tee logs
     cat << EOF | ./avtomat_conv.py 2>&1 | tee --append logs
