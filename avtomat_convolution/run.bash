3296704de8fdc322c854cc0d9fe95f76127d344c
Steven Mackey <stevester94@gmail.com>
2021-06-02 20:07:42 -0700

    Remove broken distance 4 from target distances

--- avtomat_convolution/run.bash
+++ avtomat_convolution/run.bash
@@ -2,15 +2,15 @@
 set -eou pipefail
 
 source_dataset_base_path="/csc500-super-repo/datasets/automated_windower"
-results_base_path="/csc500-super-repo/csc500-past-runs"
+results_base_path="/csc500-super-repo/csc500-past-runs/oracle_avtomat/"
 
 original_batch_size=100
 patience=10
 
 for epochs in 10 300; do
 for learning_rate in 0.0001 0.001; do
+for distance in 8 14 20 26 44 62 2 32 38 50 56; do # distance 4 is busted
 for desired_batch_size in 128 256 512; do
-for distance in 2 4 8 14 20 26 32 38 44 50 56 62; do
     experiment_name=avtomat_distance-${distance}_learningRate-${learning_rate}_batch-${desired_batch_size}_epochs-${epochs}_patience-$patience
     echo "Begin $experiment_name" | tee logs
     cat << EOF | ./avtomat_conv.py 2>&1 | tee --append logs
